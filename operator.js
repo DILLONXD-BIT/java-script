bf1f94338de2741c209d560e4e9c371130793693
DILLONXD-BIT <lugoloobidillon@gmail.com>
2025-04-16 11:41:50 +0300

    java classes

---
+++ operator.js
@@ -0,0 +1,45 @@
+// this is the addition operation
+let x=10
+let y=11
+console.log(x+y)
+let firstnames="Edward"
+let secondnames="Darwin"
+console.log(firstnames+" "+secondnames)
+// this is multiplication and subtruction and division
+let q=12
+let r=5
+console.log(q-r)
+let f=16
+let s=12
+console.log(f*s)
+let p=9
+let o=(9+8)*p
+console.log(p/o)
+// this is an exponantial
+let t=3
+// modulus
+console.log(t**6)
+let m=6
+let b=4
+console.log(m%b)
+// increament and decrement
+let k=23
+k++
+console.log(k)
+let g=21
+g--
+console.log(g)
+//assignment operators
+let u=8
+u+=5
+console.log(u)
+u-=4
+console.log(u)
+u/=2
+console.log(u)
+u*=2
+console.log(u)
+u%=3
+console.log(u)
+u**=2
+console.log(u)
---
+++ variables.js
@@ -0,0 +1,34 @@
+let firstnames="Dillon"
+console.log(firstnames)
+let myhome="i live at Lubowa"
+console.log(myhome)
+let myage="i am 2o years old"
+console.log(myage)
+let myeducation="i am an s6 leaver and i am currently studying coding"
+console.log(myeducation)
+let myhobbies="i play football, I draw, I make music, I play chess"
+console.log(myhobbies)
+let mycarrier="i want to be an artist and actor"
+console.log(mycarrier)
+let myreligion="i am a seventhday adventist christian"
+console.log(myreligion)
+let firstnumber=24
+console.log(firstnumber)
+let secondnumber=78
+console.log(secondnumber)
+let thirdnumber=613
+console.log(thirdnumber)
+console.log(secondnumber+thirdnumber)
+const mysex="male"
+console.log(mysex)
+const mydateofbith="27th july 2004"
+console.log(mydateofbith)
+const myskincolor="black"
+console.log(myskincolor)
+const sabbathday="saturday"
+console.log(sabbathday)
+const nameoflion="lion"
+console.log(nameoflion)
+const mycountry="kenya"
+secondnumber=43
+console.log(secondnumber)
